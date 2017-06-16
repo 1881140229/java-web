ab5681eb0e4883c8a8f2e6e71574aea3b1d312a6
1881140229 <1932197300@qq.com>
2017-06-16 12:03:13 -0500

    src

---
+++ DBBean.java
@@ -0,0 +1,66 @@
+package com.krieg;
+
+/**
+ * Created by Krieg on 2017/6/10.
+ */
+import java.sql.*;
+public class DBBean {
+    private String driverStr = "com.mysql.jdbc.Driver";//数据库驱动
+    private String connStr = "jdbc:mysql://localhost:3306/test";//数据库地址
+    private String dbusername = "root";//数据库用户名
+    private String dbpassword = "123456";//数据库密码
+    private Connection conn = null;
+    private Statement stmt = null;
+
+    public DBBean()
+    {
+        try
+        {
+            Class.forName(driverStr);
+            conn = DriverManager.getConnection(connStr, dbusername, dbpassword);
+            stmt = conn.createStatement();
+        }
+        catch (Exception ex) {
+            System.out.println("数据库连接失败！");
+        }
+
+    }
+
+    public int executeUpdate(String s) {
+        int result = 0;
+        System.out.println("--更新语句:"+s+"\n");
+        try {
+            result = stmt.executeUpdate(s);
+        } catch (Exception ex) {
+            System.out.println("执行更新错误！");
+        }
+        return result;
+    }
+
+    public ResultSet executeQuery(String s) {
+        ResultSet rs = null;
+        System.out.print("--查询语句:"+s+"\n");
+        try {
+            rs = stmt.executeQuery(s);
+        } catch (Exception ex) {
+            System.out.println("ִ执行查询错误！");
+        }
+        return rs;
+    }
+    public void execQuery(String s){
+        try {
+            stmt.executeUpdate(s);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("执行插入错误！");
+        }
+    }
+
+    public void close() {
+        try {
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+        }
+    }
+}
